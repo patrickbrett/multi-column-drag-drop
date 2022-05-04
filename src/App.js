6e5b3b981987795301a5567224ad2abdb098bad0
Patrick Brett <patrick@neara.com>
2022-05-04 22:28:42 +1000

    Make an add menu

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import "./App.css";
 import React, { useState } from "react";
 import useMouse from "@react-hook/mouse-position";
 
-const toolsList = ["A", "B", "C", "D", "E", "F", "G", "H"].map((name, i) => ({
-  name,
-  i,
-}));
+const toolsList = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"].map(
+  (name, i) => ({
+    name,
+    i,
+  })
+);
 
 // what if we could also drag below the bottom one?
 
@@ -29,7 +31,7 @@ function FloatingTool({ mouse, active, offset }) {
   );
 }
 
-function Tool({ active, data, handleDown, handleUp }) {
+function Tool({ active, data, handleDown, handleUp, removeThisTool }) {
   const ref = React.useRef();
   const mouse = useMouse(ref);
 
@@ -38,28 +40,82 @@ function Tool({ active, data, handleDown, handleUp }) {
       ref={ref}
       key={data.i}
       className={`tool ${active === null ? "" : "dragging"}`}
-      onMouseDown={() => handleDown(data, { x: mouse.x, y: mouse.y })}
       onMouseUp={() => handleUp(data)}
     >
+      {data.name}
+      <div
+        className="move-button"
+        onMouseDown={() => handleDown(data, { x: mouse.x, y: mouse.y })}
+      >
+        Move
+      </div>
+      <div
+        className="remove-button"
+        onClick={(e) => {
+          e.stopPropagation();
+          removeThisTool();
+        }}
+      >
+        -
+      </div>
+    </div>
+  );
+}
+
+function AddTool({ data, enableThisTool }) {
+  return (
+    <div key={data.i} className="tool add-tool" onClick={enableThisTool}>
       {data.name}
     </div>
   );
 }
 
+function AddButton({ enabledTools, setEnabled, setPositions }) {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const enableTool = (tool) => {
+    console.log('en', tool);
+    // Put the just-enabled tool last
+    setPositions((prev) => {
+      const newPositions = prev.filter(pos => pos !== tool.i);
+      newPositions.push(tool.i);
+      return newPositions;
+    });
+
+    setEnabled((prev) => {
+      return prev.map((enabled, i) => (i === tool.i ? true : enabled));
+    });
+  };
+
+  return (
+    <>
+      <div key="add" className={`tool`} onClick={() => setMenuOpen(true)}>
+        Add
+      </div>
+      {isMenuOpen && (
+        <div className="add-tools">
+          {toolsList
+            .filter((tool) => !enabledTools[tool.i])
+            .map((tool) => (
+              <AddTool data={tool} enableThisTool={() => enableTool(tool)} />
+            ))}
+        </div>
+      )}
+    </>
+  );
+}
+
 function App() {
   // 1, 0, 2, 3, 4, 5, 6, 7 means B, A, ....
-  // So if pos[i] = j that means element j is at position i.
+  // So if pos[i] = j that means tool j is at position i.
 
   const [positions, setPositions] = useState([0, 1, 2, 3, 4, 5, 6, 7]);
   const [active, setActive] = useState(null);
   const [offset, setOffset] = useState({ x: 0, y: 0 });
+  const [enabled, setEnabled] = useState(toolsList.map((_, i) => i % 2 === 0));
 
   const ref = React.useRef(null);
   const mouse = useMouse(ref, { enterDelay: 100, leaveDelay: 100 });
-
-  const list = positions.map((position) =>
-    toolsList.find((tool) => tool.i === position)
-  );
 
   const handleDown = (tool, clickOffset) => {
     console.log(
@@ -77,8 +133,11 @@ function App() {
   };
 
   const handleUp = (tool) => {
+    if (active === null) {
+      return;
+    }
+
     setPositions((positions) => {
-      console.log(positions, active.i);
       const newPositions = positions.slice(0);
       newPositions.splice(
         positions.findIndex((pos) => pos === tool.i),
@@ -91,6 +150,28 @@ function App() {
     setActive(null);
   };
 
+  const removeTool = (tool) => {
+    console.log("remove");
+    setEnabled((prev) => {
+      return prev.map((enabled, i) => (i === tool.i ? false : enabled));
+    });
+  };
+
+  const list = positions
+    .map((position) => toolsList.find((tool) => tool.i === position))
+    .filter((tool) => enabled[tool.i])
+    .map((data) => (
+      <Tool
+        active={active}
+        data={data}
+        handleDown={handleDown}
+        handleUp={handleUp}
+        removeThisTool={() => removeTool(data)}
+      />
+    ));
+
+  list.push(<AddButton setPositions={setPositions} enabledTools={enabled} setEnabled={setEnabled} />);
+
   const first = list.slice(0, 5);
   const second = list.slice(5);
 
@@ -99,16 +180,8 @@ function App() {
       {active !== null && (
         <FloatingTool mouse={mouse} active={active} offset={offset} />
       )}
-      <div className="toolbar">
-        {first.map((data) => (
-          <Tool active={active} data={data} handleDown={handleDown} handleUp={handleUp} />
-        ))}
-      </div>
-      <div className="toolbar">
-        {second.map((data) => (
-          <Tool active={active} data={data} handleDown={handleDown} handleUp={handleUp} />
-        ))}
-      </div>
+      <div className="toolbar">{first}</div>
+      <div className="toolbar">{second}</div>
     </div>
   );
 }
